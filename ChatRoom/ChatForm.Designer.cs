db0714102a09e5744e04391fb1e47d98ed5c420b
HexaPath <43265617+HexaPath@users.noreply.github.com>
2018-10-28 11:10:08 +0100

    Design

--- ChatRoom/ChatForm.Designer.cs
+++ ChatRoom/ChatForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnProfil = new System.Windows.Forms.Button();
             this.searchBox = new System.Windows.Forms.TextBox();
             this.Status_button_online = new System.Windows.Forms.Button();
@@ -36,11 +37,30 @@
             this.button1 = new System.Windows.Forms.Button();
             this.MessagetextBox = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnProfil
             // 
-            this.btnProfil.Location = new System.Drawing.Point(519, 8);
+            this.btnProfil.Location = new System.Drawing.Point(685, 3);
             this.btnProfil.Name = "btnProfil";
             this.btnProfil.Size = new System.Drawing.Size(64, 64);
             this.btnProfil.TabIndex = 5;
@@ -49,17 +69,17 @@
             // 
             // searchBox
             // 
-            this.searchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.searchBox.Location = new System.Drawing.Point(12, 12);
+            this.searchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.searchBox.Location = new System.Drawing.Point(3, 4);
             this.searchBox.Name = "searchBox";
-            this.searchBox.Size = new System.Drawing.Size(185, 30);
+            this.searchBox.Size = new System.Drawing.Size(287, 38);
             this.searchBox.TabIndex = 7;
             // 
             // Status_button_online
             // 
             this.Status_button_online.BackColor = System.Drawing.Color.Lime;
             this.Status_button_online.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.Status_button_online.Location = new System.Drawing.Point(589, 12);
+            this.Status_button_online.Location = new System.Drawing.Point(757, 4);
             this.Status_button_online.Name = "Status_button_online";
             this.Status_button_online.Size = new System.Drawing.Size(16, 16);
             this.Status_button_online.TabIndex = 8;
@@ -70,7 +90,7 @@
             // 
             this.Status_button_inactive.BackColor = System.Drawing.Color.Yellow;
             this.Status_button_inactive.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.Status_button_inactive.Location = new System.Drawing.Point(589, 34);
+            this.Status_button_inactive.Location = new System.Drawing.Point(757, 26);
             this.Status_button_inactive.Name = "Status_button_inactive";
             this.Status_button_inactive.Size = new System.Drawing.Size(16, 16);
             this.Status_button_inactive.TabIndex = 9;
@@ -81,7 +101,7 @@
             // 
             this.Status_button_offline.BackColor = System.Drawing.Color.Red;
             this.Status_button_offline.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.Status_button_offline.Location = new System.Drawing.Point(589, 56);
+            this.Status_button_offline.Location = new System.Drawing.Point(757, 48);
             this.Status_button_offline.Name = "Status_button_offline";
             this.Status_button_offline.Size = new System.Drawing.Size(16, 16);
             this.Status_button_offline.TabIndex = 10;
@@ -90,9 +110,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 48);
+            this.button1.Location = new System.Drawing.Point(3, 73);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(185, 51);
+            this.button1.Size = new System.Drawing.Size(287, 51);
             this.button1.TabIndex = 11;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -100,35 +120,196 @@
             // MessagetextBox
             // 
             this.MessagetextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.MessagetextBox.Location = new System.Drawing.Point(203, 412);
+            this.MessagetextBox.Location = new System.Drawing.Point(498, 541);
             this.MessagetextBox.Name = "MessagetextBox";
-            this.MessagetextBox.Size = new System.Drawing.Size(364, 30);
+            this.MessagetextBox.Size = new System.Drawing.Size(525, 30);
             this.MessagetextBox.TabIndex = 12;
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(573, 410);
+            this.btnSend.Location = new System.Drawing.Point(1029, 539);
             this.btnSend.Name = "btnSend";
             this.btnSend.Size = new System.Drawing.Size(32, 32);
             this.btnSend.TabIndex = 13;
             this.btnSend.Text = "Sent";
             this.btnSend.UseVisualStyleBackColor = true;
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.vScrollBar1);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.searchBox);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(321, 583);
+            this.panel1.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnProfil);
+            this.panel2.Controls.Add(this.Status_button_online);
+            this.panel2.Controls.Add(this.Status_button_inactive);
+            this.panel2.Controls.Add(this.Status_button_offline);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(321, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(752, 70);
+            this.panel2.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(287, 51);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 244);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(287, 51);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 187);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(287, 51);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 472);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(287, 51);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(3, 415);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(287, 51);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(3, 358);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(287, 51);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(3, 301);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(287, 51);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(3, 529);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(287, 51);
+            this.button9.TabIndex = 19;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(293, 73);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(22, 507);
+            this.vScrollBar1.TabIndex = 20;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Lime;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button10.Location = new System.Drawing.Point(3, 3);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(12, 12);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Profil";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Yellow;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button11.Location = new System.Drawing.Point(21, 3);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(12, 12);
+            this.button11.TabIndex = 21;
+            this.button11.Text = "Profil";
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Red;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button12.Location = new System.Drawing.Point(39, 3);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(12, 12);
+            this.button12.TabIndex = 22;
+            this.button12.Text = "Profil";
+            this.button12.UseVisualStyleBackColor = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button12);
+            this.panel3.Controls.Add(this.button10);
+            this.panel3.Controls.Add(this.button11);
+            this.panel3.Location = new System.Drawing.Point(12, 49);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(54, 18);
+            this.panel3.TabIndex = 24;
+            // 
             // ChatForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(617, 450);
+            this.ClientSize = new System.Drawing.Size(1073, 583);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.MessagetextBox);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.Status_button_offline);
-            this.Controls.Add(this.Status_button_inactive);
-            this.Controls.Add(this.Status_button_online);
-            this.Controls.Add(this.searchBox);
-            this.Controls.Add(this.btnProfil);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ChatForm";
             this.Text = "ChatForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -143,5 +324,21 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox MessagetextBox;
         private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
